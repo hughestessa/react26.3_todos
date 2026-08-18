88aa602ffc01683ffa4a0de0d851b0d1d51db71c
Tessa Hughes <147353819+hughestessa@users.noreply.github.com>
2026-08-18 00:15:19 -0500

    Moved files around

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import TodoForm from './TodoForm.jsx';
-import TodoList from './TodoList.jsx';
+import TodoForm from './features/TodoForm.jsx';
+import TodoList from './features/TodoList/TodoList.jsx';
 import {useState} from 'react';
 
 /* const todos = [
