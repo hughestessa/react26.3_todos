fbb30277bf7589322964b348aaf89003111a6923
Tessa Hughes <147353819+hughestessa@users.noreply.github.com>
2026-08-26 01:10:45 -0500

    Lesson07 half way through part 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 
 import './App.css';
+import {useState} from 'react';
 import Header from './shared/Header.jsx';
 import TodosPage from './features/Todos/TodosPage.jsx';
+import Logon from './features/Logon.jsx';
 
 
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
+  
   return(
     <div>
-      <Header />
-      <TodosPage />
+      <Header 
+        token={token}
+        onSetToken={setToken}
+        onSetEmail={setEmail}
+      />
+
+      {token ? 
+        <TodosPage token={token} /> : 
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      }
+      
     </div>
   )
 
--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,13 +1,54 @@
 
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList/TodoList.jsx';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 
-function TodosPage() {
+function TodosPage({token}) {
 
   const [todoList, setTodoList] = useState([]);
+  const [error, SetError] = useState('');
+  const [isTodoListLoading, setIsTodoListLoading] = useState(false);
 
+  useEffect(() => {
+    if (!token) return;
+    async function fetchTodos(){
+      setError('');
+      setIsTodoListLoading(true);
+
+      try{
+        const params = new URLSearchParams({
+        limit: 100,
+        });
+        const response = await fetch(`/api/tasks?${params}`, {
+        headers: {'X-CSRF-TOKEN': token,},
+        credentials: 'include',
+        });
+
+        if (response.status === 401) {
+          throw new Error('unauthorized');
+        }
+        
+        if(!response.ok){
+          throw new Error (`Failed to fetch tasks. Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setTodoList(data);
+        
+      }
+      catch(err){
+        SetError(err.message)
+      }
+      finally{
+        setIsTodoListLoading(false);
+      }
+    }
+
+    fetchTodos;
+  },[token]);
+
+  
   function addTodo(todoTitle){
       const newTodo = {
         id: Date.now(),
@@ -48,4 +89,4 @@ function TodosPage() {
       
 }
 
-export default TodosPage
+export default TodosPage;
